f52daaab3e25d21a4c6abcb594c361377c8a6b37
hknunes <hknunes@gmail.com>
2023-11-24 16:26:36 +0000

    Added Filter div, missing logic

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from 'react';
 import Numbers from './components/Numbers';
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -40,9 +46,24 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with:{' '}
+        <input
+          type="search"
+          value={filter}
+          onChange={handleFilterChange}
+        ></input>
+      </div>
+
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -56,10 +77,6 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <Numbers persons={persons} />
-
-      <div>
-        debug: {newName} {newNumber}
-      </div>
     </div>
   );
 };
