2d989a5e671a70af7ae2e86d721bb0057826b1a4
hknunes <hknunes@gmail.com>
2023-12-08 11:09:03 +0000

    ex 3.12

--- part3/backend/mongo.js
+++ part3/backend/mongo.js
@@ -7,24 +7,34 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://hknunes:${password}@cluster0.5mcdgmw.mongodb.net/phoneBook?retryWrites=true&w=majority`;
 
 mongoose.set('strictQuery', false);
 mongoose.connect(url);
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 });
 
-const Note = mongoose.model('Note', noteSchema);
+const Person = mongoose.model('Person', personSchema);
 
-const note = new Note({
-  content: 'HTML is Easy',
-  important: true,
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
 });
 
-note.save().then((result) => {
-  console.log('note saved!');
-  mongoose.connection.close();
-});
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log('phonebook:');
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  person.save().then((result) => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
